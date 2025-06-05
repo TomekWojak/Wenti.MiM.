e9ec176f4cdd87094e53d56a76083b63e046f7dd
Tomek <tomek.wojak796@gmail.com>
2025-06-05 17:48:37 +0200

    added stats counter

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+	// ADDING SVG ICONS TO NAV SECTION
 	const airConditioningImg = document.querySelector(
 		".nav__link-img-conditioning"
 	);
-	const recuperationImg = document.querySelector(
-		".nav__link-img-recuperation"
-	);
-	const wodkanImg = document.querySelector(
-		".nav__link-img-wodkan"
-	);
+	const recuperationImg = document.querySelector(".nav__link-img-recuperation");
+	const wodkanImg = document.querySelector(".nav__link-img-wodkan");
 	fetch("./images/svg/fan-icon.svg")
 		.then((res) => res.text())
 		.then((data) => {
@@ -26,4 +23,36 @@ document.addEventListener("DOMContentLoaded", function () {
 			wodkanImg.innerHTML = data;
 		})
 		.catch((err) => console.error(err));
+
+	// ADDING SVG ICONS TO NAV SECTION
+
+	const counterItems = document.querySelectorAll(".counter__stats-stat");
+	const counterBox = document.querySelector(".counter");
+
+	const options = {
+		rootMargin: "-250px",
+	};
+	const startCounter = (entry) => {
+		if (entry[0].isIntersecting) {
+			counterItems.forEach((counter) => {
+				const updateCounter = () => {
+					const finalNumber = counter.getAttribute("data-number");
+					const value = parseInt(counter.textContent);
+
+					const speed = 2;
+
+					if (value < finalNumber) {
+						counter.textContent = `${Math.floor(value + speed)}`;
+						setTimeout(updateCounter, 5);
+					} else {
+						counter.textContent = finalNumber;
+					}
+				};
+				updateCounter();
+			});
+		}
+	};
+
+	const observer = new IntersectionObserver(startCounter, options);
+	observer.observe(counterBox);
 });
