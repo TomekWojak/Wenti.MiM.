c4b76252d778e941b375866fffbcd09409c61f22
Tomek <tomek.wojak796@gmail.com>
2025-07-11 15:12:47 +0200

    changed a little bit structure of website for better seo

--- js/subpage-script.js
+++ js/subpage-script.js
@@ -27,4 +27,50 @@ document.addEventListener("DOMContentLoaded", function () {
 	backToTopArrowBtn.addEventListener("click", () => {
 		window.scrollTo({ top: 0 });
 	});
+
+	// ACCORDION
+	const allAccordionBtns = document.querySelectorAll(
+		".questions__accordion-toggler"
+	);
+
+	allAccordionBtns.forEach((toggler) => {
+		toggler.addEventListener("click", () => {
+			const box = toggler.closest(".questions__accordion-box");
+			const answer = box.querySelector(".questions__accordion-answer");
+
+			const isOpen = answer.style.height && answer.style.height !== "0px";
+			document
+				.querySelectorAll(".questions__accordion-answer")
+				.forEach((otherAnswer) => {
+					if (otherAnswer !== answer) {
+						otherAnswer.setAttribute("aria-hidden", "true");
+						otherAnswer.style.height = otherAnswer.scrollHeight + "px";
+						requestAnimationFrame(() => {
+							otherAnswer.style.height = "0";
+						});
+
+						const otherToggler = otherAnswer
+							.closest(".questions__accordion-box")
+							.querySelector(".questions__accordion-toggler");
+						otherToggler.classList.remove("opened");
+						otherToggler.setAttribute("aria-expanded", "false");
+					}
+				});
+
+			if (isOpen) {
+				answer.setAttribute("aria-hidden", "true");
+				toggler.setAttribute("aria-expanded", "false");
+				toggler.classList.remove("opened");
+				answer.style.height = answer.scrollHeight + "px";
+				requestAnimationFrame(() => {
+					answer.style.height = "0";
+				});
+			} else {
+				answer.setAttribute("aria-hidden", "false");
+				toggler.setAttribute("aria-expanded", "true");
+				toggler.classList.add("opened");
+				answer.style.height = answer.scrollHeight + "px";
+			}
+		});
+	});
 });
